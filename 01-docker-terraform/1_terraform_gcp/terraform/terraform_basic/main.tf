c00b3fb1b7301056a8dff6e9bbf0da486c2e1926
cibuitrago123 <ciaransb1@gmail.com>
2025-08-20 02:31:12 +0000

    init

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -8,24 +8,22 @@ terraform {
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "<Your Project ID>"
+  project = "sublime-iridium-469404-n8"
   region  = "us-central1"
 }
 
 
 
 resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "<Your Unique Bucket Name>"
-  location      = "US"
+  name     = "sublime-iridium-bucket"
+  location = "US"
 
   # Optional, but recommended settings:
-  storage_class = "STANDARD"
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 
   versioning {
-    enabled     = true
+    enabled = true
   }
 
   lifecycle_rule {
@@ -33,7 +31,7 @@ resource "google_storage_bucket" "data-lake-bucket" {
       type = "Delete"
     }
     condition {
-      age = 30  // days
+      age = 30 // days
     }
   }
 
@@ -42,7 +40,5 @@ resource "google_storage_bucket" "data-lake-bucket" {
 
 
 resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "<Your Project ID>"
-  location   = "US"
+  dataset_id = "sublime_iridium_dataset"
 }
