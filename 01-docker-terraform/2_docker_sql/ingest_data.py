7b391a8423d7e8fa1da352c55f172f825f9f226a
cibuitrago123 <ciaransb1@gmail.com>
2025-08-20 18:32:05 +0000

    add column verification to ingest_data.py

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -34,8 +34,9 @@ def main(params):
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    if "tpep_pickup_datetime" in df.columns and "tpep_dropoff_datetime" in df.columns: 
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
@@ -49,8 +50,9 @@ def main(params):
             
             df = next(df_iter)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            if "tpep_pickup_datetime" in df.columns and "tpep_dropoff_datetime" in df.columns: 
+                df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+                df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
